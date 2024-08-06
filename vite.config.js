5727d8cfbe4f71cbb6f2f5f9d65893e32f2f7ddd
José Edberto Santos de França <edbertosantos.dev@gmail.com>
2024-08-06 04:10:02 -0300

    configuracao de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://EdbertoSantos.github.io/edberto_portifolio_react"
 })
