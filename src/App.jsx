2da88bc8adbdbb416ce033ed5a0f732ce5174157
EdbertoSantos <edbertosantos62@gmail.com>
2024-10-31 03:47:26 -0300

    atualização modal pagina projeto

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import { Outlet } from 'react-router-dom'
 
 import Navbar from './assets/components/Navbar'
-import Footer from './assets/components/Footer'
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
     <div>
       <Navbar />
       <Outlet />
-      <Footer />
     </div>
   )
 }
--- src/assets/routes/Projetos.jsx
+++ src/assets/routes/Projetos.jsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 import calcIMC from "../IMG/calcIMC.png"
 import clickSAUDE from "../IMG/clickSAUDE.png"
 import calc from '../IMG/calc.png'
-import Modal from "../components/Modal.jsx"
 import ia from "../IMG/hacktonIA.png"
+
+import Modal from "../components/Modal.jsx"
+import IMC from "../components/readMe/IMC.jsx";
+import Calculadora from "../components/readMe/Calculadora.jsx"
 
 import '../css/Projetos.css'
 
 const Projetos = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const [modalFrameProject, setModalFrameProject] = useState('');
+    const [modalContent, setModalContent] = useState(null);
 
-    const [openModal, setOpenModal] = useState(false)
-    const [modalFrameProject, setModalFrameProject] = useState('')
-    const [modalFrameReadMe, setModalFrameReadMe] = useState('')
-    const [modalFrameLink, setModalFrameLink] = useState('')
-
-    const handleOpenModal = (frameProject, frameReadMe, frameLink) => {
+    const handleOpenModal = (frameProject, content) => {
         setModalFrameProject(frameProject);
-        setModalFrameReadMe(frameReadMe);
-        setModalFrameLink(frameLink)
+        setModalContent(content);
         setOpenModal(true);
     };
 
@@ -30,32 +30,30 @@ const Projetos = () => {
                     <h1 className="Project-Space-Title">Projetos!</h1>
                     <nav className="Project-Space-Nav">
                         <ul className="Project-Space-Nav-Ul-Destaque"> {/*Destaque*/}
-                            <li className="Project-Space-Nav-Ul-Li-Destaque">    {/**/}
-                                <Link to='' onClick={() => { handleOpenModal('', 'src/assets/pdfs/imc.pdf', '', '') }}>
+                            <li className="Project-Space-Nav-Ul-Li-Destaque">
+                                <Link to='' onClick={() => { handleOpenModal('', 'Destaque') }}>
                                     <img src={ia} alt="Projetos-IMG" className="Projetos-Img" />
                                 </Link>
                             </li>
                         </ul>
                         <ul className="Project-Space-Nav-Ul"> {/*Html, css e Js*/}
                             <li className="Project-Space-Nav-Ul-Li">    {/*Calculadora Imc*/}
-                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/Calculadora_Imc/', 'src/assets/pdfs/imc.pdf', 'https://github.com/EdbertoSantos/Calculadora_Imc?tab=readme-ov-file') }}>
+                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/Calculadora_Imc/', <IMC />) }}>
                                     <img src={calcIMC} alt="Projetos-IMG" className="Projetos-Img" />
                                 </Link>
                             </li>
                             <li className="Project-Space-Nav-Ul-Li">    {/*Click Saude*/}
-                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/click-saude/', 'src/assets/pdfs/clicksaude.pdf', 'https://github.com/EdbertoSantos/click-saude') }}>
+                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/click-saude/', <Calculadora />) }}>
                                     <img src={clickSAUDE} alt="Projetos-IMG" className="Projetos-Img" />
                                 </Link>
                             </li>
                             <li className="Project-Space-Nav-Ul-Li">    {/*Calculadora*/}
-                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/calculadora/', 'src/assets/pdfs/calculadora.pdf', 'https://github.com/EdbertoSantos/calculadora') }}>
+                                <Link to='' onClick={() => { handleOpenModal('https://edbertosantos.github.io/calculadora/', <Calculadora />) }}>
                                     <img src={calc} alt="Projetos-IMG" className="Projetos-Img" />
                                 </Link>
                             </li>
                         </ul>
-                        <ul className="Project-Space-Nav-Ul">
-                        </ul>
-                        {openModal && <Modal closeModal={setOpenModal} frameProject={modalFrameProject} frameReadMe={modalFrameReadMe} frameLink={modalFrameLink} />}
+                        {openModal && <Modal closeModal={setOpenModal} frameProject={modalFrameProject} modalContent={modalContent} />}
                     </nav>
                 </div>
             </section>
